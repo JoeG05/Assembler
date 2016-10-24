591e1c80b4f744a7bd39b21791b98124365f991b
JoeG05 <jgualdar@ramapo.edu>
2016-10-24 15:08:16 -0400

    Initial Commit

---
+++ stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+// TODO: reference additional headers your program requires here
+// Standard include files.
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <windows.h>
+#include <map>
+
+using namespace std;
+
